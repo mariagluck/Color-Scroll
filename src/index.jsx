ed5f2f3eab84f5fc7bfec43f04fab5929a894544
mariagluck <mariamart.berlin@gmail.com>
2021-10-12 15:02:35 +0200

    edit

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDom from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 
 
 ReactDom.render(
-    <BrowserRouter>
+    <React.StrictMode>
     <App />
-    </BrowserRouter>,
+    </React.StrictMode>,
      document.querySelector("#root")
 );
