ce0953f6491c46420fd15fc5cfdc11711a441035
mariagluck <mariamart.berlin@gmail.com>
2021-10-12 14:56:27 +0200

    predeploy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,7 @@ export default function App() {
     return (
         <main>
             <Switch>
-                <Route exact path="/">
+                <Route>
                 <Home /> 
                 <Scroll />   
                 </Route> 
